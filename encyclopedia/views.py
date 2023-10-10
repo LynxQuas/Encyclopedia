1c758418b60533b68d6b29f688507785017add21
LynxQuas <lynxquas@gmail.com>
2023-10-10 20:52:37 +0630

    added the search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -54,6 +54,38 @@ def search(request):
                     "message": "Page does not exist."
             })
 
+# New page
+def newpage(request):
+    if request.method == "POST":
+        title = request.POST.get("title", "")
+        content = request.POST.get("content", "")
+        if not title or not content:
+            return render(request, "encyclopedia/error.html", {
+                    "title": "something went wrong",
+                    "message": "Input is empty"
+            })
+        existing_entry = util.get_entry(title)
+        if existing_entry:
+            return render(request, "encyclopedia/error.html", {
+                    "title": "something went wrong",
+                    "message": "Page Already Exist."
+            })
+        else:
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/entryPage.html", {
+                "title": title,
+                "content":markdowner.convert(util.get_entry(title))
+       })
+    else:
+         return render(request, "encyclopedia/newpage.html")
+
+         
+
+
+    
+
+   
+
             
         
         
